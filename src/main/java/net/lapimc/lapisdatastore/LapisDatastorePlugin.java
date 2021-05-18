d9ca5efa1379014c2685aa5762f7a991e13f49b5
Benjamin Martin <DNAblue2112@gmail.com>
2021-05-18 21:49:14 +0930

    Added auto updater

--- src/main/java/net/lapimc/lapisdatastore/LapisDatastorePlugin.java
+++ src/main/java/net/lapimc/lapisdatastore/LapisDatastorePlugin.java
@@ -1,11 +1,19 @@
 package net.lapimc.lapisdatastore;
 
 import net.lapismc.lapiscore.LapisCorePlugin;
+import net.lapismc.lapiscore.utils.LapisUpdater;
+import org.bukkit.Bukkit;
 
 public final class LapisDatastorePlugin extends LapisCorePlugin {
 
     @Override
     public void onEnable() {
-
+        Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
+            String name = getDescription().getName();
+            LapisUpdater updater = new LapisUpdater(this, name, "Dart2112", name, "master");
+            if (updater.checkUpdate()) {
+                updater.downloadUpdate();
+            }
+        });
     }
 }
