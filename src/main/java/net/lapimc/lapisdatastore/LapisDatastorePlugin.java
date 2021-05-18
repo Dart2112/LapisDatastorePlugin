6eaa829db2297a7636c96695ccd2f43cd9f5804d
Benjamin Martin <DNAblue2112@gmail.com>
2021-05-18 21:45:42 +0930

    Initial commit

---
+++ src/main/java/net/lapimc/lapisdatastore/LapisDatastorePlugin.java
@@ -0,0 +1,11 @@
+package net.lapimc.lapisdatastore;
+
+import net.lapismc.lapiscore.LapisCorePlugin;
+
+public final class LapisDatastorePlugin extends LapisCorePlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
